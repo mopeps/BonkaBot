f66c1290f40fb07412e6f61ad6f3b56501fca2f2
mopeps <lucagiabb@gmail.com>
2021-03-10 00:43:43 +0000

    added marco polo

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const Discord = require("discord.js")
+const fetch = require("node-fetch")
+const Database = require("@replit/database")
+
+const db = new Database()
 const client = new Discord.Client()
+
+db
+
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.login(proccess.env.TOKEN);
+client.login(process.env.TOKEN);
 //listening to msgs
 client.on('message', msg => {
-	console.log(msg.content);
+  if (msg.content === 'marco' || msg.content === 'Marco') {
+    msg.channel.send('Poloo!');
+  } 
 });
+
+
+
